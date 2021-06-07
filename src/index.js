98652a4c4a7a3303defae43907295ed2e2379916
Fabrice Defay <defay.fabrice@gmail.com>
2021-06-06 21:07:07 -0400

    changed app to a class component. Added axios

--- src/index.js
+++ src/index.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import { render } from 'react-dom';
+import axios from 'axios';
 import FrequentQuestions from './frequentQuestions/FrequentQuestions.js';
 import Overview from './overview/Overview.js';
-import Ratings from './ratings/Ratings.js';
-import RelatedItems from './relatedItems/RelatedItems.js';
-import Search from './Search.js';
+import Ratings from './ratings/Ratings';
+import RelatedItems from './relatedItems/RelatedItems';
+import Search from './Search';
+import { API_KEY } from '../config';
 
-function App() {
+class App extends React.Component() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: undefined,
+    };
+  }
 
-  return (
-    <>
-    <Search />
-    <Overview />
-    <RelatedItems />
-    <FrequentQuestions />
-    <Ratings />
-    </>
-    )
+  componentDidMount() {
+    console.log('running compnent did mount');
+    this.fetchProduct();
+  }
 
+  fetchProduct() {
+    console.log('fetching product');
+    const config = {
+      headers: {
+        Autorization: API_KEY,
+      },
+    };
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/19089', config)
+      .then((data) => console.log(data))
+      .catch((err) => console.log('error getting product.... ', err));
+  }
+
+  render() {
+    console.log('running render');
+
+    return (
+      <>
+        <Search />
+        <Overview />
+        <RelatedItems />
+        <FrequentQuestions />
+        <Ratings />
+      </>
+    );
+  }
 }
 
-render (<App />, document.getElementById('react-root'));
+render(<App />, document.getElementById('react-root'));
