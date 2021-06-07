92c15e373dca2f5eb8dcd50be800949fb9b4b769
Fabrice Defay <defay.fabrice@gmail.com>
2021-06-07 19:54:29 -0400

    display answers in expanding accordion style

--- src/frequentQuestions/Answer.jsx
+++ src/frequentQuestions/Answer.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default ({answer}) => {
   return (
-    <p><strong>A: </strong>{answer.body}</p>
+    <>
+      <p><strong>A: </strong>{answer.body}</p>
+      <p>by {answer.answerer_name} | Helpful? Yes({answer.helpfulness}) | Report</p>
+    </>
   )
 }
--- src/frequentQuestions/Question.jsx
+++ src/frequentQuestions/Question.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Answer from './Answer.jsx'
 
 const Question = ({question}) => {
+  const [numberOfVisibleAnswers, setNumberOfVisibleAnswers] = useState(2)
   const answers = Object.values(question.answers)
+  let visibleAnswers = answers.slice(0, numberOfVisibleAnswers)
+
+  function loadMoreAnswers() {
+    setNumberOfVisibleAnswers(numberOfVisibleAnswers + 2)
+  }
+
   return (
     <section>
       <strong>Q: {question.question_body}</strong>
       <section>
-        {answers.map(answer => <Answer key={answer.id} answer={answer}/>)}
+        {visibleAnswers.map(answer => <Answer key={answer.id} answer={answer}/>)}
       </section>
+      <strong onClick={loadMoreAnswers}>Load More Answers</strong>
     </section>
   )
 }
