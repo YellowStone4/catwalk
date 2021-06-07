1da860b86932b3d35056afd2638ab17c2d0a55e5
Zachary Sawyer <zacharysawyer@Zacharys-MacBook-Pro.local>
2021-06-07 09:58:02 -0400

    set up basic style and structure for overview component

--- src/overview/Overview.js
+++ src/overview/Overview.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import styles from './overviewStyles.js';
+import ProductDetails from './components/ProductDetails';
+import Gallery from './components/Gallery';
+import Selection from './components/Selection';
+
 
 function Overview() {
-  return <h2>Overview: Zack</h2>
+  return (
+    <section style={styles.sectionElement}>
+      <div style={styles.productContainer} id="productContainer">
+        <Gallery />
+        <Selection />
+
+      </div>
+      <ProductDetails />
+
+    </section>
+  )
 }
 
 
---
+++ src/overview/components/Gallery.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const containerStyle = {
+  backgroundColor: '#d1d9d9'
+}
+
+
+function Gallery() {
+  return (
+    <div style={containerStyle}>
+
+    </div>
+  )
+}
+
+export default Gallery;
