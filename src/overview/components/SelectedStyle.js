84c7e5af82affe6bd46332007f21ac3afdb5ef39
Zachary Sawyer <zacharysawyer@Zacharys-MacBook-Pro.local>
2021-06-13 18:40:20 -0400

    More styling

--- src/overview/components/SelectedStyle.js
+++ src/overview/components/SelectedStyle.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css';
+import clsx from 'clsx';
 
 function SelectedStyle({setCurrentStyle, productStyles, ...rest}) {
+  const [checkedStyle, setCheckedStyle] = useState(0);
+
+  function handleClick(style, i) {
+    setCurrentStyle(style);
+    setCheckedStyle(i);
+  }
+
   return (
     <div className='styleSelection'>
       <p><b>STYLE ></b> SELECTED STYLE</p>
         <div className='productStyles'>
           {
             productStyles.map((style, i) => {
-              return <div className='styleIcons' key={i} onClick={() => setCurrentStyle(style)} style={{backgroundImage:`url(${style.photos[0].thumbnail_url})`}} />
+              let classes = clsx({selectedInput: i === checkedStyle})
+              return (
+              <div className='styleIconWrapper'>
+                  {/* <input type="radio" name="radioStyle" value="small"/> */}
+                  <div className='styleIcons' key={i} onClick={() => handleClick(style, i)} style={{backgroundImage:`url(${style.photos[0].thumbnail_url})`}} />
+                  <span class={classes}>
+                    <div className={clsx({checkCircle: i === checkedStyle })}></div>
+                    <div className={clsx({checkStem: i === checkedStyle })}></div>
+                    <div className={clsx({checkKick: i === checkedStyle })}></div>
+                  </span>
+              </div>
+              )
             })
           }
 
