2133e4f47e2d9d3a622301affa31444b0525fedd
Fabrice Defay <defay.fabrice@gmail.com>
2021-06-15 17:41:05 -0400

    add question modal viewable but not functioning

--- src/frequentQuestions/Search.jsx
+++ src/frequentQuestions/Search.jsx
@@ -1,16 +1,14 @@
 import React, {useState} from 'react'
 
 export default ({search}) => {
-
   const handleChange = (e) => {
     if(e.target.value.length >= 3) {
       search(e.target.value)
     } else {
       search('')
     }
-
   }
   return (
-    <input type="text" placeholder="Search For Question" onChange={handleChange}/>
+    <input type="text" placeholder="Search For Questions" onChange={handleChange}/>
   )
 }
