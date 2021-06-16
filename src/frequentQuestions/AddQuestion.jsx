e69fef50e6fa62e2f219850d8d5a251d86e5f457
Fabrice Defay <defay.fabrice@gmail.com>
2021-06-16 09:09:46 -0400

    OnChange method set for form

--- src/frequentQuestions/AddQuestion.jsx
+++ src/frequentQuestions/AddQuestion.jsx
@@ -1,22 +1,42 @@
 import React, {useState} from 'react'
 
 export default ({submit, cancel, product}) => {
-  const [question, setQuestion] = useState('')
+  const [formData, setFormData] = useState({
+    question: '',
+    nickname: '',
+    email: ''
+  })
+  // const [question, setQuestion] = useState('')
   const [questionIsValid, setQuestionIsValid] = useState(true)
-  const [nickname, setNickname] = useState('')
+  // const [nickname, setNickname] = useState('')
   const [nicknameIsValid, setNicknameIsValid] = useState(true)
-  const [email, setEmail] = useState('')
+  // const [email, setEmail] = useState('')
   const [emailIsValid, setEmailIsValid] = useState(true)
 
   const validate = () => {
     if (question.length === 0) {
-      return setQuestionIsValid(false)
+      setQuestionIsValid(false)
     }
+    if (nickname.length === 0) {
+      setNicknameIsValid(false)
+    }
+    if (!validateEmail(email)) {
+      return setEmailIsValid(false)
+    }
+    submit()
   }
 
   const validateEmail = (email) => {
     return false
   }
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <div className="modal" onClick={cancel}>
       <div className="modal-content">
@@ -25,17 +45,41 @@ export default ({submit, cancel, product}) => {
         <form>
           <label>
             *Your Question:
-            <input name="question" type="text" placeholder="Your Question" value={question} maxlength="1000"/>
-            {!questionIsValid && <p>question cannot be empty</p>}
+            <input
+              name="question"
+              type="text"
+              placeholder="Your Question"
+              value={formData.question}
+              maxLength="1000"
+              onChange={onChange}
+            />
+            {!questionIsValid && <span className="alert">Question cannot be empty</span>}
           </label>
+          <br />
           <label>
             *What is your nickname:
-            <input name="nickname" type="text" placeholder="Example: jackson11!" value={nickname} maxlength="60"/>
+            <input
+              name="nickname"
+              type="text"
+              placeholder="Example: jackson11!"
+              value={formData.nickname}
+              maxLength="60"
+              onChange={onChange}
+            />
+            {!nicknameIsValid && <p className="alert">Nickname cannot be empty</p>}
             <p>For privacy reasons, do not use your full name or email address</p>
           </label>
           <label>
             *Your email:
-            <input name="email" type="text" placeholder="Why did you like the product or not?" value={email} maxlength="60" />
+            <input
+              name="email"
+              type="text"
+              placeholder="Why did you like the product or not?"
+              value={formData.email}
+              maxLength="60"
+              onChange={onChange}
+            />
+            {!emailIsValid && <p className="alert">email is not valid</p>}
             <p>For authentication reasons, you will not be emailed</p>
           </label>
 
